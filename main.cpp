161275a32729c027442d2a6266d0548ac0eab98a
Tinyiko Mbidhli <mbidhlitinyikoamukelane@gmail.com>
2026-07-02 07:09:48 +0200

    Implement student records manager in main.cpp

---
+++ main.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+
+
+const int MAX = 100; //our storage limit
+string names[MAX];
+string ids[MAX];
+double grades[MAX];
+int counT = 0; // number of student stored.
+
+
+void addStudent()
+{
+    if (counT >= 100)
+    {
+        cout << "STORAGE FULL" << endl;
+        cout << endl;
+        return;
+    }
+
+    cout << "ENTER STUDENT NAME: ";
+    cin.ignore();
+    getline(cin, names[counT]);
+    
+    cout << "ENTER STUDENT ID: ";
+    cin >> ids[counT];
+
+    cout << "ENTER GRADE (0 - 100): ";
+    cin >> grades[counT];
+
+    counT++;
+}
+
+void displayAll()
+    {
+    if (counT == 0)
+    {
+        cout << "NO STUDENTS ON RECORD" << endl;
+        return;
+    }
+
+    cout << "\nALL STUDENTS:\n" << endl;
+    for (int i = 0; i < counT; i++)
+    {
+        cout << i + 1 << ". " << names[i] << " | ID: " << ids[i] << " | Grade: " << grades[i] << "%" << endl;
+    } 
+
+    }
+
+
+
+
+int main()
+{
+    int response = 0;
+    cout << "<<< STUDENT RECORDS MANAGER >>> \n" << endl;
+
+    do
+    {
+        cout << "\n\nMAIN MENU\n\n" << endl;
+        cout << "\n1. ADD STUDENT\n2. DISPLAY ALL FILES\n3. EXIT\n\n" << endl;
+        cin >> response;
+        cout << endl;
+
+        if (response == 1)
+
+        {
+            addStudent();
+            cout << endl;
+        }
+
+        else if (response == 2)
+        {
+            displayAll();
+            cout << endl;
+        }
+
+    } while (response != 3);
+    
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
